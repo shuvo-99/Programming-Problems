d968ee91456a89bb95ede3d329161e139bfa63be
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2022-11-05 12:39:43 +0600

    Update Find the Closest prime number.py

--- Find the Closest prime number.py
+++ Find the Closest prime number.py
@@ -1,3 +1,35 @@
+# Only return the closest prime number
+
+def closestPrimeNum(num):
+    
+    for i in range(1,num):
+        flag = False    
+        n = num + i
+        for j in range(2,n):
+            if (n % j) == 0:
+                flag = True
+                break
+        
+        if flag ==  False:
+            return f"{n} is prime number."
+            
+        flag = False
+        n = num - i
+        if n>1:
+            for j in range(2,n):
+                if (n % j) == 0:
+                    flag = True
+                    break
+            if flag ==  False:
+                return f"{n} is prime number."
+
+print(closestPrimeNum(10))
+
+
+
+
+# Print all the closest prime numbers
+
 num = 1001
 
 for i in range(1,num):
@@ -20,3 +52,6 @@ for i in range(1,num):
                 break
         if flag ==  False:
             print(n, "is prime number")
+ 
+
+ 
